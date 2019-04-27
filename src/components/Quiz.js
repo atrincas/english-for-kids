e19135d000f6763d88771b1f44fbc52589035ecc
Adam Trincas <atrincas@gmail.com>
2019-04-27 21:08:21 +0200

    Added default state, added createNewQuestion function

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,7 +5,26 @@ import data from "../data/data";
 class Quiz extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			nr: 0,
+			total: data.length,
+			score: 0,
+			finsihed: false
+		};
+	}
+
+	componentWillMount() {
+		let { nr } = this.state;
+		this.createNewQuestion(nr);
+	}
+
+	createNewQuestion(nr) {
+		this.setState({
+			currentWord: data[nr].word,
+			img: data[nr].img,
+			options: data[nr].options,
+			nr: this.state.nr + 1
+		});
 	}
 
 	render() {
