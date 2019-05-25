5af705ac5b72b098c8cbc17b75d82f14bc0609a0
Adam Trincas <atrincas@gmail.com>
2019-05-25 12:41:18 +0200

    Removed links from sections in navbar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -23,59 +23,61 @@ class LandingPage extends React.Component {
           <div className="LPcontainer" />
         </div>
         <p className="welcome">Welcome to English For Kids</p>
-        <div className="row">
-          <Section            
-            onClick={this.setSection}
-            id={"colors"}
-            alt={"colors"}
-            src={colors}
-          >
-            Colors
-          </Section>
-          <Section            
-            onClick={this.setSection}
-            id={"fruits"}
-            alt={"fruits"}
-            src={fruits}
-          >
-            Fruits
-          </Section>
-        </div>
-        <div className="row">
-          <Section            
-            onClick={this.setSection}
-            id={"clothes"}
-            alt={"clothes"}
-            src={clothes}
-          >
-            Clothes
-          </Section>
-          <Section            
-            onClick={this.setSection}
-            id={"drinks"}
-            alt={"drinks"}
-            src={drinks}
-          >
-            Drinks
-          </Section>
-        </div>
-        <div className="row">
-          <Section            
-            onClick={this.setSection}
-            id={"animals"}
-            alt={"animals"}
-            src={animals}
-          >
-            Animals
-          </Section>
-          <Section            
-            onClick={this.setSection}
-            id={"partsOfBody"}
-            alt={"partsOfBody"}
-            src={partsOfBody}
-          >
-            Parts of Body
-          </Section>
+        <div id="sections">
+          <div className="row">
+            <Section            
+              onClick={this.setSection}
+              id={"colors"}
+              alt={"colors"}
+              src={colors}
+            >
+              Colors
+            </Section>
+            <Section            
+              onClick={this.setSection}
+              id={"fruits"}
+              alt={"fruits"}
+              src={fruits}
+            >
+              Fruits
+            </Section>
+          </div>
+          <div className="row">
+            <Section            
+              onClick={this.setSection}
+              id={"clothes"}
+              alt={"clothes"}
+              src={clothes}
+            >
+              Clothes
+            </Section>
+            <Section            
+              onClick={this.setSection}
+              id={"drinks"}
+              alt={"drinks"}
+              src={drinks}
+            >
+              Drinks
+            </Section>
+          </div>
+          <div className="row">
+            <Section            
+              onClick={this.setSection}
+              id={"animals"}
+              alt={"animals"}
+              src={animals}
+            >
+              Animals
+            </Section>
+            <Section            
+              onClick={this.setSection}
+              id={"partsOfBody"}
+              alt={"partsOfBody"}
+              src={partsOfBody}
+            >
+              Parts of Body
+            </Section>
+          </div>
         </div>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import * as setLandingPageAction from "../state/actions/setLandingPage";
@@ -13,6 +13,7 @@ class Navbar extends React.Component {
     return (
       <div className="nav">
         <a href="/">Home</a>
+        <a href="#sections">Sections</a>
         <a
           href="https://github.com/atrincas/english-for-kids/blob/master/README.md"
           target="_blank"
@@ -20,66 +21,6 @@ class Navbar extends React.Component {
         >
           About
         </a>
-        <div className="dropdown">
-          <button className="dropbtn" style={{ width: "50%" }}>
-            Sections
-          </button>
-          <div className="dropdown-content">
-            <Link
-              to="/quiz"
-              onClick={this.setSection}
-              id="colors"
-              style={{ color: "black" }}
-            >
-              Colors
-            </Link>
-            <br />
-            <Link
-              to="/quiz"
-              onClick={this.setSection}
-              id="fruits"
-              style={{ color: "black" }}
-            >
-              Fruits
-            </Link>
-            <br />
-            <Link
-              to="/quiz"
-              onClick={this.setSection}
-              id="clothes"
-              style={{ color: "black" }}
-            >
-              Clothes
-            </Link>
-            <br />
-            <Link
-              to="/quiz"
-              onClick={this.setSection}
-              id="drinks"
-              style={{ color: "black" }}
-            >
-              Drinks
-            </Link>
-            <br />
-            <Link
-              to="/quiz"
-              onClick={this.setSection}
-              id="animals"
-              style={{ color: "black" }}
-            >
-              Animals
-            </Link>
-            <br />
-            <Link
-              to="/quiz"
-              onClick={this.setSection}
-              id="partsOfBody"
-              style={{ color: "black" }}
-            >
-              PartsofBody
-            </Link>
-          </div>
-        </div>
       </div>
     );
   }
