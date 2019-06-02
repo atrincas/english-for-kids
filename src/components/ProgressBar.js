0d039b4d6ef0edd1c2d8fb2a3582a76c5987e184
Adam Trincas <atrincas@gmail.com>
2019-06-02 06:01:46 +0200

    Added ProgressBar component

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProgressBar = (props) => {
+    return (
+        <div className="progress-bar">
+            <div className="filler" style={{width: `${props.percentage}%`}}></div>
+        </div>
+    )
+}
